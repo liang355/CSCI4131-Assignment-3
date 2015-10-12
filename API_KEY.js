93147d2461a82a6d7efa002b42bd696a16ff1083
Ben Liang <liang355@umn.edu>
2015-10-11 23:06:35 -0500

    commit

---
+++ API_KEY.js
@@ -0,0 +1,7 @@
+/**
+ * Created by yingbinliang on 10/11/15.
+ */
+
+//API_KEY for Google Maps API:
+
+var API_KEY = "AIzaSyA4PVk2eGoaHzQ1W1Xvlr2QilbGNM_xNtw";
---
+++ map.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yingbinliang on 10/11/15.
+ */
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
